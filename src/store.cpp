56375e48ee508af70bf39c49b38a4b39b68099bf
Raja Rokkam <rrokkam@chitika.com>
2012-10-25 14:18:14 +0530

    Added hour field to log file names.

--- src/store.cpp
+++ src/store.cpp
@@ -446,7 +446,8 @@ string FileStoreBase::makeBaseFilename(struct tm* creation_time) {
   if (rollPeriod != ROLL_NEVER) {
     filename << '-' << creation_time->tm_year + 1900  << '-'
              << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '-'
-             << setw(2) << setfill('0')  << creation_time->tm_mday;
+             << setw(2) << setfill('0')  << creation_time->tm_mday << '_' 
+             << setw(2) << setfill('0')  << creation_time->tm_hour;
 
   }
   return filename.str();
